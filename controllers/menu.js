4eb05d915a884bb05a8442421aea507a41850f75
rigov1984 <rigov18@hotmail.com>
2021-03-03 18:06:57 -0500

    Se agrego el endpoint para eliminar menu

--- controllers/menu.js
+++ controllers/menu.js
@@ -46,8 +46,69 @@ function getMenus(req, res) {
 
 }
 
+//actualiza el menu
+function updateMenu(req, res) {
+    let menuData = req.body;
+    const params = req.params;
+
+    Menu.findByIdAndUpdate(params.id, menuData, (err, menuUpdate) => {
+        if (err) {
+            res.status(500).send({ message: "Error del servidor." });
+        } else {
+            if (!menuUpdate) {
+                res.status(404).send({ message: "No se ha encontrado ningun menu." });
+            } else {
+                res.status(200).send({ message: "Menu actualizado correctamente." });
+            }
+        }
+    })
+}
+
+//activa y desactiva el menu
+function activateMenu(req, res) {
+    const { id } = req.params;
+    const { active } = req.body;
+
+
+    Menu.findByIdAndUpdate(id, { active }, (err, menusStored) => {
+        if (err) {
+            res.status(500).send({ message: "Error de servidor." });
+        } else {
+            if (!menusStored) {
+                res.status(404).send({ message: "No se ha encontrado el menu." });
+            } else {
+                if (active == true) {
+                    res.status(200).send({ message: "Menu activado correctamente." });
+                } else {
+                    res.status(200).send({ message: "Menu desactivado correctamente." });
+
+                }
+            }
+        }
+    })
+}
+
+function deleteMenu(req, res) {
+    const { id } = req.params;
+
+    Menu.findByIdAndRemove(id, (err, menuDeleted) => {
+        if (err) {
+            res.status(500).send({ message: "Error del servidor." })
+        } else {
+            if (!menuDeleted) {
+                res.status(404).send({ message: "Menu no encontrado." })
+            } else {
+                res.status(200).send({ message: "El menu ha sido eliminado correctamente." })
+            }
+        }
+    })
+
+}
 
 module.exports = {
     addMenu
     , getMenus
+    , updateMenu
+    , activateMenu
+    , deleteMenu
 }
--- routers/menu.js
+++ routers/menu.js
@@ -12,7 +12,9 @@ const api = express.Router();
 //iniciamos nuestras router de express
 api.post("/add-menu", [md_auth.ensureAuth], MenuControler.addMenu);
 api.get("/get-menus", MenuControler.getMenus);
-
+api.put("/update-menu/:id", [md_auth.ensureAuth], MenuControler.updateMenu);
+api.put("/activate-menu/:id", [md_auth.ensureAuth], MenuControler.activateMenu);
+api.delete("/delete-menu/:id", [md_auth.ensureAuth], MenuControler.deleteMenu);
 //estamos exportando api por lo que todas las rutas tambien son importadas.
 module.exports = api
 
